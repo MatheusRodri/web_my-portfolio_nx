63ca0be65c5c0712aabb3c7258f0c429d934fb9b
Matheus Rodrigues De Jesus <matheus.rj25@hotmail.com>
2025-06-20 13:44:30 -0300

    update local header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import styles from '@/components/styles.module.css';
+import styles from '@/components/header/styles.module.css';
 
 export default function Header() {
     const [menuOpen, setMenuOpen] = useState(false);
