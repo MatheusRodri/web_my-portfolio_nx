1441ecd07804f6178a0f2d200a75e300b7fe8c74
Matheus Rodrigues De Jesus <matheus.rj25@hotmail.com>
2025-06-21 20:30:32 -0300

    Create About Section

--- src/components/Sections/aboutSection/index.tsx
+++ src/components/Sections/aboutSection/index.tsx
@@ -2,8 +2,15 @@ import style from './style.module.css';
 
 export function AboutSection() {
     return (
-        <>
-            <p>teste</p>
-        </>
+        <section id="about" className={style.aboutSection}>
+            <div className={style.aboutContent}>
+                <h2 className={style.aboutTitle}>About Me</h2>
+                <p className={style.aboutText}>
+                    Hello! I'm Matheus, a passionate software developer with a focus on creating innovative solutions.
+                    I have experience in various programming languages and frameworks, and I love tackling complex challenges.
+                    My goal is to build applications that not only meet user needs but also provide an exceptional user experience.
+                </p>
+            </div>
+        </section>
     )
 }
