09aa3f28decea5ccb232066c3507b440e85699c4
Matheus Rodrigues De Jesus <matheus.rj25@hotmail.com>
2025-06-21 20:29:29 -0300

    add About Section and Skill Section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import { AboutSection } from "@/components/Sections/aboutSection";
 import InitialSection from "@/components/Sections/InitialSection";
+import SkillSection from "@/components/Sections/skillSection";
 
 export default function Home() {
   return (
     <main>
       <InitialSection />
-      <AboutSection/>
+      <AboutSection />
+      <SkillSection/>
     </main>
   );
 }
