4549f9c61ed32a9ed4d7b81d01ec4b17259aeeea
Matheus Rodrigues De Jesus <matheus.rj25@hotmail.com>
2025-06-22 16:02:41 -0300

    create Contact Section

---
+++ src/sections/contactSection/index.tsx
@@ -0,0 +1,28 @@
+import Link from 'next/link';
+import style from './style.module.css';
+import data from '@/data/data.json';
+
+export function ContactSection() {
+    return (
+        <section id='contact' className={style.contactSection}>
+            <h2 className={style.contactTitle}>Contato</h2>
+            <ul className={style.contactList}>
+                <li className={style.contactItem}>
+                    <Link href={`mailto:${data.contact_user.email}`} className={style.contactLink}>
+                        <span className={style.contactIcon}>📧 Email: {data.contact_user.email}</span>
+                    </Link>
+                </li>
+                <li className={style.contactItem}>
+                    <Link href={data.contact_user.linkedin} className={style.contactLink}>
+                        <span className={style.contactIcon}>🔗 LinkedIn</span>
+                    </Link>
+                </li>
+                <li className={style.contactItem}>
+                    <Link href={data.contact_user.github} className={style.contactLink}>
+                        <span className={style.contactIcon}>🐱 GitHub</span>
+                    </Link>
+                </li>
+            </ul>
+        </section>
+    )
+}
